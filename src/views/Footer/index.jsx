a56739834dc118a15d343969322fd7ef91b64cc6
Daniel <danilog1000@gmail.com>
2022-02-10 22:00:23 -0500

    desktop screen style

--- src/views/Footer/index.jsx
+++ src/views/Footer/index.jsx
@@ -9,22 +9,24 @@ export default function Footer(props){
 
   return(
     <footer className={`${style.container_footer} ${hideSection ? style.hide : style.show}`}>
-      <TextFooter 
-        icon={<FontAwesomeIcon className={style.font_icon} icon="clock"/>} 
-        text="Lun-Dom: 6:00am-4pm"
-      />
-      <TextFooter 
-        icon={<FontAwesomeIcon className={style.font_icon} icon="phone-alt"/>} 
-        text="3043327794" 
-      />
-      <TextFooter 
-        icon={<FontAwesomeIcon className={style.font_icon} icon="envelope"/>} 
-        text="verdurasmilena2605@gmail.com" 
-      />
-      <TextFooter
-        icon={<FontAwesomeIcon className={style.font_icon} icon="map-marker-alt"/>} 
-        text={<>Plaza Paloquemao<br/>Local 83067</>} 
-      />
+      <div className={`${style.container_items}`}>
+        <TextFooter 
+          icon={<FontAwesomeIcon className={style.font_icon} icon="clock"/>} 
+          text="Lun-Dom: 6:00am-4pm"
+        />
+        <TextFooter 
+          icon={<FontAwesomeIcon className={style.font_icon} icon="phone-alt"/>} 
+          text="3043327794" 
+        />
+        <TextFooter 
+          icon={<FontAwesomeIcon className={style.font_icon} icon="envelope"/>} 
+          text="verdurasmilena2605@gmail.com" 
+        />
+        <TextFooter
+          icon={<FontAwesomeIcon className={style.font_icon} icon="map-marker-alt"/>} 
+          text={<>Plaza Paloquemao<br/>Local 83067</>} 
+        />
+      </div>
       <p className={style.rights}>Todos los derechos reservados © 2022</p>
     </footer>
   );
